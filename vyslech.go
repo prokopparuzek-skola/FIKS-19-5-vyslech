a570c6c729df3c0ca7d69fa494bfcfb349beefa4
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-04-09 21:11:40 +0200

    init

---
+++ vyslech.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var T int
+	fmt.Scan(&T)
+	for ; T > 0; T-- {
+		var N, K int
+		fmt.Scan(&N, &K)
+		for i := 0; i < N; i++ {
+			var M int
+			fmt.Scan(&M)
+			for j := 0; j < M; j++ {
+			}
+		}
+	}
+}
